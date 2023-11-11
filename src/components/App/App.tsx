1bf716185e1facb4dc58f7a6f55247c5db53d29e
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 13:58:12 +0100

    Add component HomePage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
+import HomePage from "../HomePage/HomePage";
 
 const App = (): React.ReactElement => {
   return (
     <div>
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </main>
     </div>
   );
 };
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import HomePageStyled from "./HomePageStyled";
+
+const HomePage = (): React.ReactElement => {
+  return (
+    <HomePageStyled>
+      <h2>All users</h2>
+    </HomePageStyled>
+  );
+};
+
+export default HomePage;
