b46a14e3fd4a14ec006c63086044c30e5352d9bc
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 13:35:23 +0100

    Add component Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  return (
+    <HeaderStyled>
+      <h1 className="heading-title">Tuttyfly</h1>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
