8a5c77e395e21e4eab46a5939416c137ad00d429
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 13:10:23 +0100

    Add component App

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
 import "@fontsource/saira-stencil-one";
 import "@fontsource-variable/montserrat";
+import App from "./components/App/App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
-      <div />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
