aaf48f770313b1e29c9f35f1e1c8a228dff3521c
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 12:39:04 +0100

    Add mainTheme style

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    background: "#30313f",
+    userCardBackground: "#535863",
+    ButtonBackground: "#2cd798",
+    mainFont: "#fff",
+    secondaryFont: "#b9bec6",
+    containerBackground: "#e5d24c",
+    disableButtonBackground: "#1bb76e",
+  },
+  typography: {
+    mainFontFamily: "'Montserrat Variable', sans-serif",
+    secondartFontFamily: "'Saira Stencil One', sans-serif",
+    propetySize: "1.2rem",
+    titleSize: "2rem",
+  },
+};
+
+export default mainTheme;
