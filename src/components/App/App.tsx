b4fd9a46f0c765b290c04a7d8cad4bdb47329440
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 16:20:33 +0100

    Revert "Feature/home page component"

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
-import HomePage from "../HomePage/HomePage";
 
 const App = (): React.ReactElement => {
   return (
     <div>
       <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </main>
+      <main></main>
     </div>
   );
 };
--- src/components/HomePage/HomePageStyled.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-const HomePageStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-export default HomePageStyled;
