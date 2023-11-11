7385284f2c167b8aa3442b9d00b9883ffd1e0c71
Matias Risco <matiasriscolagos@gmail.com>
2023-11-11 12:42:35 +0100

    Add import fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
+import "@fontsource/saira-stencil-one";
+import "@fontsource-variable/montserrat";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
